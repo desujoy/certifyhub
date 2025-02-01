bb7ea0c8b2003c366799b8c2ff6894b973ec8c5f
Harshit Saini <saini457harshit@gmail.com>
2025-02-01 20:46:16 +0530

    Optimized contract visibility to external

--- backend/src/CertifyHub.sol
+++ backend/src/CertifyHub.sol
@@ -11,20 +11,17 @@ contract CertifyHub {
 
     event certificateGenerated(bytes32 certificate_id);
 
-    function generateCertificate(string memory _candidate_name, string memory _template) public {
+    function generateCertificate(string memory _candidate_name, string memory _template) external {
         bytes32 _certificate_id = keccak256(abi.encodePacked(_candidate_name, _template));
-        require(
-            bytes(certificates[_certificate_id].candidate_name).length == 0, "Certificate with this ID already exists"
-        );
+        require(bytes(certificates[_certificate_id].candidate_name).length == 0, "Certificate with this ID already exists");
 
         Certificate memory cert = Certificate({candidate_name: _candidate_name, template: _template});
-
         certificates[_certificate_id] = cert;
 
         emit certificateGenerated(_certificate_id);
     }
 
-    function generateMultiCertificate(string[] memory _candidate_name, string memory _template) public {
+    function generateMultiCertificate(string[] memory _candidate_name, string memory _template) external {
         for (uint256 i = 0; i < _candidate_name.length; i++) {
             bytes32 _certificate_id = keccak256(abi.encodePacked(_candidate_name[i], _template));
 
@@ -33,28 +30,21 @@ contract CertifyHub {
             }
 
             Certificate memory cert = Certificate({candidate_name: _candidate_name[i], template: _template});
-
             certificates[_certificate_id] = cert;
 
             emit certificateGenerated(_certificate_id);
         }
     }
 
-    function getCertificate(bytes32 _certificate_id)
-        public
-        view
-        returns (string memory _candidate_name, string memory _template)
-    {
+    function getCertificate(bytes32 _certificate_id) external view returns (string memory _candidate_name, string memory _template) {
         Certificate memory cert = certificates[_certificate_id];
 
-        require(
-            bytes(certificates[_certificate_id].candidate_name).length != 0, "Certificate with this ID does not exist"
-        );
+        require(bytes(certificates[_certificate_id].candidate_name).length != 0, "Certificate with this ID does not exist");
 
         return (cert.candidate_name, cert.template);
     }
 
-    function isVerified(bytes32 _certificate_id) public view returns (bool) {
+    function isVerified(bytes32 _certificate_id) external view returns (bool) {
         return bytes(certificates[_certificate_id].candidate_name).length != 0;
     }
 }
